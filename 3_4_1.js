7ae4ef237227d85717d71c22b79e35bce12377a7
HarveySpecter-coder <greenfreedom7@yandex.ru>
2023-01-09 20:37:58 +0300

    1

---
+++ 3_4_1.js
@@ -0,0 +1,44 @@
+function ElectricalAppliance(name, power) {
+    this.name = name;
+    this.power = power;
+    this.pluggedIn = false;
+}
+
+ElectricalAppliance.prototype.plugIn = function() {
+    this.pluggedIn = true;
+    console.log(this.name + " is plugged");
+};
+
+ElectricalAppliance.prototype.unplug = function() {
+    this.pluggedIn = false;
+    console.log(this.name + " is unplugged");
+};
+
+function Fridge(name, brand, power, madeIn) {
+    this.name = name;
+    this.brand = brand;
+    this.power = power;
+    this.madeIn = madeIn;
+    this.pluggedIn = false;
+}
+
+function Router(name, brand, power, wifi, module4G) {
+    this.name = name;
+    this.brand = brand;
+    this.power = power;
+    this.wifi = false;
+    this.module4G = false;
+    this.pluggedIn = false;
+}
+
+Fridge.prototype = new ElectricalAppliance();
+Router.prototype = new ElectricalAppliance();
+
+const fridge = new Fridge("Fridge", "Indesit", 520, "China");
+const router = new Router("Router", "Huawei", 60, true, false);
+
+fridge.unplug();
+router.plugIn();
+
+console.log(router)
+console.log(fridge)
